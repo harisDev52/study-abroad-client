aa7bb40be7c7e891bb512df4fcdd2b7b6b3275bf
hfoster050 <hfoster050@gmail.com>
2024-05-30 10:22:19 +0500

    new changes

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,3 @@
 // export const API_ENDPOINT = 'http://localhost:5000'
-export const API_ENDPOINT = 'http://170.64.228.126:5000'
+// export const API_ENDPOINT = 'http://170.64.228.126:5000'
+export const API_ENDPOINT = ''
