7997e0c15ed38318979f9e2792da3482d9641cdb
Usama Khan <usamyik16@gmail.com>
2024-05-30 05:13:02 +0500

    initial commit

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,2 +1,2 @@
-export const API_ENDPOINT = 'http://127.0.0.1:5000'
-// export const API_ENDPOINT = 'http://170.64.228.126:5000'
+// export const API_ENDPOINT = 'http://127.0.0.1:5000'
+export const API_ENDPOINT = 'http://170.64.228.126:5000'
